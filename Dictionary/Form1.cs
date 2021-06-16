90ab559a0d3db0f0fc257324f772e50b3978da56
Nguyễn Đức Mạnh <45529373+ducmanh1808@users.noreply.github.com>
2021-06-17 02:57:41 +0700

    Upload file

---
+++ Dictionary/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    public partial class Dictionary : Form
+    {
+        public Dictionary()
+        {
+            InitializeComponent();
+            WoDMeaningLb.Text = "acvc.\n ákdk";
+        }
+
+        private void SearchBox_Enter(object sender, EventArgs e)
+        {
+            if(SearchBox.Text == "Search some word...")
+            {
+                SearchBox.Text = "";
+                SearchBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void SearchBox_Leave(object sender, EventArgs e)
+        {
+            if(SearchBox.Text == "")
+            {
+                SearchBox.Text = "Search some word...";
+                SearchBox.ForeColor = Color.Silver;
+            }
+        }
+    }
+}
